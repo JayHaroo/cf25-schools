bc3cb3faecbcb498075b04af0e5a71ce373aaf24
M-Jey <mjeyict3@gmail.com>
2024-07-23 09:32:22 +0800

    update layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,32 @@ import purdue from "./assets/purdue.jpg";
 import unlv from "./assets/unlv.jpg";
 import duke from "./assets/duke.jpg";
 import logo from "./assets/logo.png";
+import lsu from "./assets/lsu.jpg";
 
 function App() {
   return (
     <div className="bg-black w-screen min-h-screen">
-      <StickyNavbar logo= {logo}/>
-      <div className="flex pl-[20px] pr-[20px] justify-between">
+      <StickyNavbar logo={logo} />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
         <SimpleCard
-          link= {purdue}
+          link={purdue}
           name="Purdue Boilermakers"
           desc="Purdue plays its home games at Ross-Ade Stadium on the campus of Purdue University in West Lafayette, Indiana."
         />
-
         <SimpleCard
-          link= {unlv}
+          link={unlv}
           name="UNLV Rebels football"
           desc="The UNLV Rebels football program is a college football team that represents the University of Nevada, Las Vegas."
         />
-
         <SimpleCard
-          link= {duke}
+          link={duke}
           name="Duke Blue Devils football"
           desc="The Duke Blue Devils football team represents Duke University in the sport of American football."
+        />
+        <SimpleCard
+          link={lsu}
+          name="LSU Tigers football"
+          desc="The LSU Tigers football program, also known as the Fighting Tigers, represents LSU in college football."
         />
       </div>
     </div>
