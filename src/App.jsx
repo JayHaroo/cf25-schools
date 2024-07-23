fad0354506ae095ca6b8c5fdea2ee9206d85cb74
M-Jey <mjeyict3@gmail.com>
2024-07-23 09:11:37 +0800

    changed path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import { StickyNavbar } from "./comp/stickyNavbar.jsx";
 import { SimpleCard } from "./comp/cards.jsx";
+import purdue from "./assets/purdue.jpg";
+import unlv from "./assets/unlv.jpg";
+import duke from "./assets/duke.jpg";
 
 function App() {
   return (
@@ -8,19 +11,19 @@ function App() {
       <StickyNavbar />
       <div className="flex pl-[20px] pr-[20px] justify-between">
         <SimpleCard
-          link="./src/assets/purdue.jpg"
+          link= {purdue}
           name="Purdue Boilermakers"
           desc="Purdue plays its home games at Ross-Ade Stadium on the campus of Purdue University in West Lafayette, Indiana."
         />
 
         <SimpleCard
-          link="./src/assets/unlv.jpg"
+          link= {unlv}
           name="UNLV Rebels football"
           desc="The UNLV Rebels football program is a college football team that represents the University of Nevada, Las Vegas."
         />
 
         <SimpleCard
-          link="./src/assets/duke.jpg"
+          link= {duke}
           name="Duke Blue Devils football"
           desc="The Duke Blue Devils football team represents Duke University in the sport of American football."
         />
