a99191f2fae2abc51de9d8650b20a988fd56f9b8
M-Jey <mjeyict3@gmail.com>
2024-07-23 09:07:01 +0800

    edit path

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,19 @@ function App() {
       <StickyNavbar />
       <div className="flex pl-[20px] pr-[20px] justify-between">
         <SimpleCard
-          link="src/assets/purdue.jpg"
+          link="./src/assets/purdue.jpg"
           name="Purdue Boilermakers"
           desc="Purdue plays its home games at Ross-Ade Stadium on the campus of Purdue University in West Lafayette, Indiana."
         />
 
         <SimpleCard
-          link="src/assets/unlv.jpg"
+          link="./src/assets/unlv.jpg"
           name="UNLV Rebels football"
           desc="The UNLV Rebels football program is a college football team that represents the University of Nevada, Las Vegas."
         />
 
         <SimpleCard
-          link="src/assets/duke.jpg"
+          link="./src/assets/duke.jpg"
           name="Duke Blue Devils football"
           desc="The Duke Blue Devils football team represents Duke University in the sport of American football."
         />
